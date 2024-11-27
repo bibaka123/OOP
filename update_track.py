4c33c2135b94ff802d2aab75077338674e1aca81
Nguyenvt <147222733+bibaka123@users.noreply.github.com>
2024-11-27 19:05:18 +0700

    Update update_track.py

--- update_track.py
+++ update_track.py
@@ -38,13 +38,40 @@ class UpdateTrack():
         self.status_lbl = tk.Label(window, text="", font=("Helvetica", 10))
         self.status_lbl.grid(row=3, column=0, columnspan=4, sticky="W", padx=10, pady=10)
 
-        update_track_btn = tk.Button(window, text="Update Playlist")
+        update_track_btn = tk.Button(window, text="Update Track", command=self.update_track_btn)
         update_track_btn.grid(row=2, column=3, rowspan=2, sticky="WE", padx=10, pady=10)
 
         self.new_rating = tk.Entry(window, width=3)
         self.new_rating.grid(row=0, column=3, padx=10, pady=10)
 
+        self.list_tracks_click()
+        self.show()
 
+    def update_track_btn(self):
+        try:
+            key = self.input_txt.get()                   
+            name = lib.get_name(key)                          
+            rating = int(self.new_rating.get())        
+            if rating <= 5:
+                if name is not None :                              
+                    lib.set_rating(key, rating)                    
+                    set_text(self.track_txt, f"Name: {lib.get_name(key)} \nNew rating: {lib.get_rating(key)}" )  
+                    self.show()
+                else:                                          
+                    msg.showwarning("Can not find tracj") 
+            else:
+                msg.showerror("Enter new rating fail")
+        except ValueError :                                   
+            msg.showerror("Rating must be an interger!")      
+
+    def list_tracks_click(self):
+        track_list = lib.list_all()  
+        set_text(self.list_txt, track_list) 
+        self.status_lbl.configure(text="List Tracks button was clicked!")
+
+    def show(self):
+        track_list = lib.list_all()                           
+        set_text(self.list_txt, track_list)  
 
 if __name__ == "__main__":  
     window = tk.Tk() 
